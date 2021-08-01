a0193f83f9b5c78c4eb567ed7a633d6a8fa6a193
JUAN-XD <88262839+CalonUstadz@users.noreply.github.com>
2021-08-01 13:17:39 +0700

    Add files via upload

---
+++ python premium.py
@@ -0,0 +1,60 @@
+@@ -1,42 +1,43 @@
+#  ✭ 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝘾𝙍𝘼𝘾𝙆
+##  ⇨   𝙁𝙚𝙖𝙩𝙪𝙧𝙚 𝙇𝙤𝙜𝙞𝙣
+#  KECEK PREMIUM
+##  ⇨   Fitur Login
+[✯] Cookie Masuk  
+[✯] Token Masuk  
+[✯] Cookie/Token Awet  
+##  ⇨   𝙁𝙚𝙖𝙩𝙪𝙧𝙚 𝘿𝙪𝙢𝙥
+##  Pembuangan   Fitur
+[✯] Buang Dari Teman  
+[✯] Buang Dari Publik  
+[✯] Buang Dari Pengikut   
+[✯] Pembuangan Cepat  
+##  ⇨ 𝙁𝙚𝙖𝙩𝙪𝙧𝙚 𝘾𝙧𝙖𝙘𝙠
+##  Fitur Useragent
+[✯] Default Agen Pengguna  
+[✯] Panduan Agen Pengguna
+##  Retak Fitur
+[✯] Retak Default  
+[✯] Panduan Retak  
+[✯] Metode Crack Mbasic  
+[✯] Metode Crack Api  
+[✯] Retak Dengan TTL  
+[✯] Retak Kata Sandi   Default 8
+[✯] Crack Default 7 Kata Sandi  
+- nama  
+- nama123  
+- nama12345  
+- anjing  
+- sayang  
+- bismillah
+- 123456
+- bangsat
+##  ⇨ 𝙄𝙣𝙨𝙩𝙖𝙡𝙡 𝙎𝙘𝙧𝙞𝙥𝙩 𝙊𝙣 𝙏𝙚𝙧𝙢𝙪𝙭
+##  Instal Script Di Termux
+$ pkg memperbarui && meningkatkan  
+$ pkg instal python2  
+$ pkg instal git  
+$pip2 instal lolcat  
+$pip2 instal bs4  
+$ pip2 permintaan pemasangan  
+$ pip2 menginstal mekanis  
+$rm -rf premium  
+$ git clone https://github.com/JUAN-XD/premium  
+##  ⇨   𝙍𝙪𝙣 𝙎𝙘𝙧𝙞𝙥𝙩
+##  ⇨   Run Script
+$cd premium  
+$ python2 premium.py
+##  ⇨   𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
+##  ⇨   Cara Gunakan
+1. Target Cari Akun
+2. Dump ID Lebih Dahulu
+3. Masukkan ID Sasaran
+@@ -45,4 +46,4 @@ $ python2 premium
+6. File Keluaran Salin
+7. Tekan Kembali, Kemudian Mulai Crack
+8. Masukkan Berkas Keluaran Tadi
+##  ⇨   𝙎𝙘𝙧𝙚𝙚𝙣𝙨𝙝𝙤𝙩
+##  ⇨   Screenshot
